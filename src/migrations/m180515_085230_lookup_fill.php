ee9b66756f9d61714baeab3d10922d380d09cd0d
Sergey Morozov <sergmoro1@ya.ru>
2018-06-01 14:14:01 +0300

    add new role - Commentator

---
+++ src/migrations/m180515_085230_lookup_fill.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180116_073828_lookup_fill
+ */
+class m180515_085230_lookup_fill extends Migration
+{
+    public function up()
+    {
+		$this->insert('{{%lookup}}', ['name' => 'Комментатор', 'code' => 3, 'type' => 'UserRole', 'position' => 3]);
+    }
+
+    public function down()
+    {
+		$this->delete('{{%lookup}}', 'type=:type AND code=3', [':type' => 'UserRole']);
+    }
+}
