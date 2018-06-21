137a6e54f3909efd0bbaacb706413a00846b5de9
Sergey Morozov <sergmoro1@ya.ru>
2018-06-21 22:47:56 +0300

    fix components/convertor  parameter type

--- src/components/convertor/Convertor.php
+++ src/components/convertor/Convertor.php
@@ -1,9 +1,7 @@
 <?php
 namespace sergmoro1\user\components\convertor;
 
-use sergmoro1\user\componets\SocialContact;
-
 interface Convertor
 {
-    public function set(SocialContact $obj, $attribute);
+    public function set($obj, $attribute);
 }
