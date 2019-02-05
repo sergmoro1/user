8851af0ec6e6efdeaa07efcfe78956f9cb2b1767
Sergey Morozov <sergmoro1@ya.ru>
2019-02-05 18:04:12 +0300

    set own urlPasswordExists for backend

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -79,7 +79,10 @@ class SiteController extends Controller
             return $this->goHome();
         }
 
-        $model = new LoginForm();
+        $model = new LoginForm([
+            'urlPasswordExists' => Url::to([
+                'user/password-exists'])
+        ]);
         if ($model->load(\Yii::$app->request->post()) && $model->login()) {
             return $this->goHome();
         } else {
